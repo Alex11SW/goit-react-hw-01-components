b92074290562cbbc32adce258a703fef3d98ea56
alex_shklyar <136641877+Alex11SW@users.noreply.github.com>
2024-01-01 16:13:31 +0200

    new

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-// import ReactDOM from 'react-dom/client';
-import { createRoot } from 'react-dom/client';
+ import ReactDOM from 'react-dom/client';
+
 import { App } from 'components/App';
 import './index.css';
 import { Profiler } from 'react';
 
 
-// ReactDOM.
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
