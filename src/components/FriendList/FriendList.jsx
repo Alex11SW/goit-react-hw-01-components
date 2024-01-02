d48097ef0204e6fcfcc962248fad89cc948971b5
alex_shklyar <136641877+Alex11SW@users.noreply.github.com>
2024-01-02 15:18:29 +0200

    new

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -7,14 +7,14 @@ const FriendList = ({ friends }) => {
     return (
         <ul className={styles['friend-list']}>
             
-              {friends.map(({ id, avatar, name, isOnline }) => (
-                    <li key={id} className={styles.item}>
-                      <span className={isOnline ? styles.online :  styles.offline}></span>
-                      <img className={styles.avatar} src={avatar} alt="User avatar" width="48" />
-                      <p className={styles.name}>{name}</p>
-                    </li>
-                ))}
-</ul>
+            {friends.map(({ id, avatar, name, isOnline }) => (
+                <li key={id} className={styles.item}>
+                    <span className={isOnline ? styles.online : styles.offline}></span>
+                    <img className={styles.avatar} src={avatar} alt="User avatar" width="48" />
+                    <p className={styles.name}>{name}</p>
+                </li>
+            ))}
+        </ul>
     );
 };
 
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,11 @@
 import user from '../../data/user.json';
 import data from '../../data/data.json';
 import friends from '../../data/friends.json';
+import transactions from '../../data/transactions.json';
 import Profile from '../Profile/Profile';
 import Statistics from 'components/Statistics/Statistics';
 import FriendList from 'components/FriendList/FriendList';
+import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
 import styles from './main.module.css';
 
 console.log(user);
@@ -19,7 +21,8 @@ console.log(user);
         />
          <Statistics title="Upload stats" stats={data} />
         {/* <Statistics stats={data} /> */}
-        <FriendList friends={friends} />;
+        <FriendList friends={friends} />
+        <TransactionHistory items={transactions} />
       </div>
      
     );
